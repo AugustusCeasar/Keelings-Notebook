2a1d6b17fbcd04326a8cc49a30e0fc961dec3302
AugustusCeasar <agustinplazareino@gmail.com>
2023-11-03 22:24:05 +0000

    fixed error with null-dates/times. Updated regex to be more general. Rewritten help.

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ find_the_truth_role_id = 1167845775875788851
 # decoding_re must contain a timestring group, where XX/XX/XXXX and XX:XX:XX are located
 # and a timezone group containing the timezone (that can only be an integer relative to GMT with current impl)
 # GMT is advised against in times of daylight savings
-time_decoding_re = re.compile(r"%(?P<timestring>.*)UTC(?P<timezone>[+-]\d\d?)?%")  # (?P<tzmode>UTC|GMT)
+time_decoding_re = re.compile(r"%(?P<timestring>.*)[Uu][Tt][Cc](?P<timezone>[+-]\d\d?)?%")  # (?P<tzmode>UTC|GMT)
 
 help_message = "Welcome to the usage of the most useful tool of the net. Its abilities are:\n" \
                "-**Find the Truth**: Automatic addition of people with a role to all threads created in certain " \
@@ -19,9 +19,13 @@ help_message = "Welcome to the usage of the most useful tool of the net. Its abi
                "two `%` and include a timezone in the form of `UCT+-XY` (or just UTC equivalently to UCT+0), " \
                "this bot will respond with a discord timestamp that will show each user the conversion of " \
                "that time to their local time. Acceptable formats for dates/times are:\n" \
-               "\t*`%DD/MM/YYYY UTC%` (D/M/YY also recognised) (if year missing current year will be assumed)\n" \
-               "\t*`%HH:MM:SS UTC%` (H:M:S recognised but discouraged)\n" \
-               "\t*`%10th UTC%` (current month/year assumed) (fluff will be ignored, eg `on the 10th` read as `10th`)" \
-               "\n\t*some combination of the above time and date\n" \
+               "\t*`%15/5/23 UTC%`\n" \
+               "\t*`%2.11 UTC+2%`\n" \
+               "\t*`%23-02-2024 UTC-1%` (always Day Month Year, sorry americans)\n" \
+               "\t*`%10th UTC-5%`\n" \
+               "\t*`%20:30 UTC+0%`\n" \
+               "\t*`%20: UTC%`\n" \
+               "\t*`%on the 2nd of this month at 15:15 UTC%` (or any text that includes 2nd/3rd/4th...)\n"  \
+               "\t*`%02/11 @ 23:55 UTC%`\n" \
                "-**Inter Server Communication**: WIP\n" \
                "-**Disable Function**: WIP\n"
--- resources/quality_time.py
+++ resources/quality_time.py
@@ -17,19 +17,20 @@ def parse_time_message(s):
     timezone_offset_parsed = datetime.timedelta(hours=int(possible_match.group("timezone")))
     # tzmode = possible_match.group("tzmode")  # either UTC or GMT as reference
 
-    parsed_date = re.search(r"(?P<day>\d\d?)((/(?P<month>\d\d?)(/(?P<year>\d\d\d?\d?))?)|(th)|(st)|(nd)|(rd))",
-                            time_string)
+    parsed_date = re.search(
+        r"(?P<day>\d\d?)(([/\-\.](?P<month>\d\d?)([/\-\.](?P<year>\d\d\d?\d?))?)|(th)|(st)|(nd)|(rd))",
+        time_string)
     parsed_time = re.search(r"(?P<hours>\d\d?):(?P<minutes>\d\d?)?(:(?P<seconds>\d\d?))?", time_string)
 
     # default dates can cause some funny behaviour around edges of timespans for some timezones
     # but when dates are left unspecified these shouldnt change anything of importance
     today = datetime.date.today()
-    day = parsed_date.group("day") or today.day
-    month = parsed_date.group("month") or today.month
-    year = parsed_date.group("year") or today.year
-    hours = parsed_time.group("hours") or 12
-    minutes = parsed_time.group("minutes") or 0
-    seconds = parsed_time.group("seconds") or 0
+    day = (parsed_date.group("day") if parsed_date else None) or today.day
+    month = (parsed_date.group("month") if parsed_date else None) or today.month
+    year = (parsed_date.group("year") if parsed_date else None) or today.year
+    hours = (parsed_time.group("hours") if parsed_time else None) or 12
+    minutes = (parsed_time.group("minutes") if parsed_time else None) or 0
+    seconds = (parsed_time.group("seconds") if parsed_time else None) or 0
     parsed_dt_before_tz = datetime.datetime(int(year), int(month), int(day), int(hours), int(minutes), int(seconds))
 
     # if tzmode == "GMT": #fuck GMT offsets
@@ -39,9 +40,9 @@ def parse_time_message(s):
     dt_after_tz_ofset = parsed_dt_before_tz - timezone_offset_parsed
 
     style = "F"
-    if parsed_time.group("hours") is None:
+    if parsed_time is None or parsed_time.group("hours") is None:
         style = "D"
-    if parsed_date.group("day") is None:
+    if parsed_date is None or parsed_date.group("day") is None:
         if parsed_time.group("seconds") is None:
             style = "t"
         else:
